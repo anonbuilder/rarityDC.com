796f564be4fb39156ce5b514a02fffc9c39a2bd3
Martin Nina <martin.nina.work@outlook.com>
2022-03-31 17:57:02 -0300

    adding useeffects and usecallback with all summoner info

--- src/App.js
+++ src/App.js
@@ -1,18 +1,67 @@
-import React from 'react'
+import {React,useCallback,  useEffect} from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useWeb3React } from '@web3-react/core'
+import { useGraphSummonerIDs } from './services/graph/hooks'
+import useRarityLibrary from './hooks/useRarityLibrary'
+import useIsWindowVisible from './hooks/useIsWindowVisible'
+
 import Summoners from './pages/Summoners'
 import NotFound from './pages/404'
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 
+const chunkArrayByNumber = (items, elements) => {
+  const res = []
+  for (let i = 0; i < items.length; i += elements) {
+      const chunk = items.slice(i, i + elements)
+      res.push(chunk)
+  }
+  return res
+}
+
 const App = () => {
+
+  const {  account, chainId, library } = useWeb3React() 
+
+  const ids = useGraphSummonerIDs(account)
+
+  const { summonersFull } = useRarityLibrary()
+
+  const windowVisible = useIsWindowVisible()
+
+  const fetchSummonersData = useCallback(
+    async (ids) => {
+        const chunks = chunkArrayByNumber(ids, 70)
+        let fetchers = []
+        for (let chunk of chunks) {
+            fetchers.push(summonersFull(chunk))
+        }
+        const fetcherChunks = chunkArrayByNumber(fetchers, 10)
+        let full_data = []
+        for (let fChunk of fetcherChunks) {
+            const chunkResponse = await Promise.all(fChunk)
+            full_data = full_data.concat(...chunkResponse)
+        }
+        const summoners_full_data = [].concat(...full_data)
+        console.log(summoners_full_data);
+      
+    },
+    [summonersFull]
+  )
+
+  useEffect(() => {
+      if (!ids || !library || !chainId || !account || !windowVisible) return
+      fetchSummonersData(ids)
+  }, [ids, windowVisible, fetchSummonersData, library, chainId, account])
+
+  
   return (
     <>
       <Header/>
       <BrowserRouter>
           <Routes>
-              <Route path="/" element={<Summoners />}   />
-              <Route element={NotFound}/>
+              <Route index element={<Summoners />}   />
+              <Route path='*' element={<NotFound/>}/>
           </Routes>
       </BrowserRouter>
       <Footer/>
