8650c9193c2dba15a47675fe11a70e25c59315fc
Martin Nina <martin.nina.work@outlook.com>
2022-04-12 11:13:52 -0300

    texts have been changed

--- src/components/BoxInfo.js
+++ src/components/BoxInfo.js
@@ -6,8 +6,8 @@ const BoxInfo = (props) => {
   return (
     <div style = {{border: '4px solid #ffffffc9',padding: '20px', margin: '50px 0', borderRadius: '0.25rem'}}>
       <div className="jumbotron">    
-        <h1 className="display-3">Hi</h1>
-        <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>        
+        <h1 className="display-3">Hi adventurer!</h1>
+        <p className="lead">Let's send you summoner to the daily adventure.</p>        
       </div>
     </div>
   );
