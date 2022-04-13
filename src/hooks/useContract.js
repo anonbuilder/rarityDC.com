f82fffdaa03b62675e961496b58ec1293d55f930
Martin Nina <martin.nina.work@outlook.com>
2022-04-13 17:02:30 -0300

    added function to interact with the adventure time contract

--- src/hooks/useContract.js
+++ src/hooks/useContract.js
@@ -5,14 +5,15 @@ import {
     RARITY_LIB,
     RARITY_ADDRESS,
     RARITY_DAYCARE_ADDRESS,
-    RARITY_DAYCARE_PLANET_ADDRESS
+    RARITY_DAYCARE_PLANET_ADDRESS,
+    RARITY_ADVENTURE_TIME
 } from '../constants/constants.js'
 
 import RARITY_ABI from '../constants/abis/rarity.json'
 import RARITY_LIB_ABI from '../constants/abis/rarity_library.json'
 import RARITY_DAYCARE_ABI from '../constants/abis/daycare.json'
 import RARITY_DAYCARE_PLANET_ABI from '../constants/abis/daycare_planet.json'
-
+import RARITY_ADVENTURE_TIME_ABI from '../constants/abis/adventure_time.json'
 
 export function useContract(address, ABI, withSignerIfPossible = true){
     const { library, account } = useWeb3React()
@@ -33,17 +34,22 @@ export function useRarityLibContract(){
     return useContract(chainId ? RARITY_LIB : undefined, RARITY_LIB_ABI)
 }
 
-export function useRarityContract(): Contract | null {
+export function useRarityContract() {
     const { chainId } = useWeb3React()
     return useContract(chainId ? RARITY_ADDRESS : undefined, RARITY_ABI)
 }
 
-export function useRarityDaycareContract(): Contract | null {
+export function useRarityDaycareContract() {
     const { chainId } = useWeb3React()
     return useContract(chainId ? RARITY_DAYCARE_ADDRESS : undefined, RARITY_DAYCARE_ABI)
 }
 
-export function useRarityDaycarePlanetContract(): Contract | null {
+export function useRarityDaycarePlanetContract() {
     const { chainId } = useWeb3React()
     return useContract(chainId ? RARITY_DAYCARE_PLANET_ADDRESS : undefined, RARITY_DAYCARE_PLANET_ABI)
+}
+
+export function useRarityAdventureTimeContract() {
+    const { chainId } = useWeb3React()
+    return useContract(chainId ? RARITY_ADVENTURE_TIME : undefined, RARITY_ADVENTURE_TIME_ABI)
 }
